9f433286dcb528db2c39951af4e3e508f79d2a29
Hamo254 <hamatonodhiambo2@gmail.com>
2022-10-19 19:46:25 +0000

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse,
+ * followed by a new line
+ * Return: 0
+*/
+int main(void)
+{
+	char h;
+
+	for (h = 'z'; h >= 'a'; h--)
+	{
+
+		putchar(h);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
