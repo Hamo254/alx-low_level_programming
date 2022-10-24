d2873882f6a413fb7d0d3f6670af47db4e169cb9
Hamo254 <hamatonodhiambo2@gmail.com>
2022-10-24 14:34:36 +0000

    created 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints the alphabet, in lowercase, a new line.
+ *
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+		_putchar(c);
+	_putchar('\n');
+}
